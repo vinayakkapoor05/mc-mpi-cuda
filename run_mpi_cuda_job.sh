fda9271798c2238582b4108f4c4e5e7ffb54a3dc
Vinayak Kapoor <72437558+vinayakkapoor05@users.noreply.github.com>
2025-05-31 23:48:11 -0500

    added job bash script

---
+++ run_mpi_cuda_job.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -A e32695
+#SBATCH -p gengpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH -N 4
+#SBATCH -n 4
+#SBATCH -t 1:00:00
+#SBATCH --mem=1G
+#SBATCH -J monte_carlo_job
+#SBATCH -o monte_carlo.out
+#SBATCH -e monte_carlo.err
+
+module purge
+module load gcc/11.2.0
+module load cuda/cuda-12.1.0-openmpi-4.1.4
+
+cd ~/monte-carlo-mpi-cuda
+
+srun ./monte_carlo_mpi_cuda
