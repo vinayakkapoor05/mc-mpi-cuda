3b3350f9c9cf8dafb8e02f5dc7ebcc03f7ab37cf
Vinayak Kapoor <72437558+vinayakkapoor05@users.noreply.github.com>
2025-05-31 23:32:40 -0500

    added time taken measurements

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include "monte_carlo.h"
-
+#include <chrono>
+#include <iostream>
 
 int main (int argc, char** argv){
     int rank, size;
@@ -10,13 +11,21 @@ int main (int argc, char** argv){
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    auto start  = std::chrono::high_resolution_clock::now();
+
     generate_random_numbers();
 
     monte_carlo_kernel(rank, size);
 
     aggregate(rank, size);
+    auto end  = std::chrono::high_resolution_clock::now();
 
     MPI_Finalize();
+
+    if (rank == 0){
+        std::chrono::duration<double> time_taken = end - start; 
+        std::cout << "Time Taken : " << time_taken.count() << " seconds\n"
+        }
     return 0;
 }
 
