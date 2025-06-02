7406456c39503b413f59d088b9563142483cc4ad
Vinayak Kapoor <72437558+vinayakkapoor05@users.noreply.github.com>
2025-06-01 20:06:50 -0500

    updated monte_carlo.h

--- include/monte_carlo.h
+++ include/monte_carlo.h
@@ -4,8 +4,8 @@
 #include <curand_kernel.h>
 
 // initialize each thread’s cuRAND state
-__global__ void setup_curand_states(curandState_t *states, unsigned long seed);
+__global__ void setup_curand_states(curandState_t *states, unsigned long seed, const long long NUM_POINTS);
 
-extern __global__ void pi_estimator_kernel(curandState_t *states, int *block_counts);
+extern __global__ void pi_estimator_kernel(curandState_t *states, int *block_counts, const long long NUM_POINTS);
 
 #endif // MONTE_CARLO_H
